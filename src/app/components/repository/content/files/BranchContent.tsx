1bac73407147e6f9bca376d0a443800e432f6a28
migo <migo.vandingo@gmail.com>
2024-04-10 21:01:48 -0600

    MIG-43: part of repo dataset. Select dataset

--- src/app/components/repository/content/files/BranchContent.tsx
+++ src/app/components/repository/content/files/BranchContent.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react"
-import styled from "styled-components"
+import { faFile } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faFile, faFolder } from "@fortawesome/free-solid-svg-icons"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import styled from "styled-components"
 import { SFlexRow } from "../../../common/containers/FlexContainers"
-import { connect, useSelector } from "react-redux"
-import { FilesAPI } from "../../../../api/FileAPI"
+import RepoSelectDataset from "./RepoSelectDataset"
+import ViewDataset from "../../../dataset/ViewDataset"
+import Subset from "../../../dataset/subset/Subset"
+import { DatasetAPI } from "../../../../api/DatasetAPI"
+import SubsetCard from "../../../dataset/subset/SubsetCard"
 
 const SRow = styled(SFlexRow)`
   align-items: center;
@@ -81,7 +85,6 @@ const SEmptyRepo = styled(SFlexRow)`
   color: ${({ theme }) => theme.color.color_3};
 `
 
-
 const BranchContent = ({
   folderPath,
   setFolderPath,
@@ -89,87 +92,36 @@ const BranchContent = ({
   projectId,
   stopSwitchFolders,
   setStopSwitchFolders,
-  folderItemsSwitch, 
+  folderItemsSwitch,
   setFolderItemsSwitch,
   files,
-  handleTriggerRender
+  handleTriggerRender,
+  show,
+  menuOption,
 }: any) => {
-  //const [files, setFiles] = useState<any[]>([])
   const [folders, setFolders] = useState<any[]>([])
   const [stopSwitchFiles, setStopSwitchFiles] = useState<boolean>(false)
+  const [selectedDataset, setSelectedDataset] = useState<any>(null)
+  const [subsets, setSubsets] = useState<any[]>([])
 
   const { repoEntity } = useSelector((state: any) => state)
 
-  
+  useEffect(() => {
+    if(selectedDataset) {
+      getSubsets(selectedDataset)
+    }
+  }, [selectedDataset]);
+
+  const getSubsets = (dataset: any) => {
+    DatasetAPI.getSubsetListByDatasetId(dataset.dataset_id)
+    .then((response) => {
+      console.log("subsets: ", response)
+      setSubsets(response.data)
+    })
+    .catch((error) => console.error("BranchContent::getSubsets()::error: ", error))
+  }
 
   let fp = []
-  /* useEffect(() => {
-    const getProjectFiles = (projectId: number) => {
-      if (projectId !== undefined && stopSwitchFiles === false) {
-        setStopSwitchFiles(true)
-
-        const response = FilesAPI.getProjectFiles(projectId)
-          .then((res: any) => {
-            setFiles(res.data)
-          })
-          .catch((err: any) => console.error(err))
-      }
-    }
-
-    const getProjectFolders = () => {
-      if (projectId !== undefined && stopSwitchFolders === false) {
-        setStopSwitchFolders(true)
-
-        const response = FilesAPI.getProjectFolders(projectId)
-          .then((res: any) => {
-            console.log("resdata: ", res.data)
-            const folderList = res.data.map((folder: any) => folder.name)
-            const f = folderList.sort()
-            setFolders(f)
-          })
-          .catch((err: any) => console.error(err))
-      }
-    }
-
-    return () => {
-      setFolderPath([])
-
-      setFiles([])
-
-      getProjectFolders()
-      
-    }
-  }, [projectId, trigger]) */
-
- /*  useEffect(() => {
-    if (folderItemsSwitch === true) {
-      console.log("folderPath: ", folderPath)
-      FilesAPI.getFolderItems(repoEntity, folderPath)
-        .then((result: any) => {
-          const files = result.data.filter((item: any) => item.type === "file")
-          
-          let folders = result.data.filter(
-            (item: any) => item.type === "folder",
-          )
-
-          //folders = folders.map((folder: any) => folder.name)
-
-
-
-          console.log("files: ", files)
-          //console.log("folders: ", folders)
-          setFolderItemsSwitch(false)
-          setFiles(files)
-          //setFolders(folders)
-        })
-        .catch((err: any) =>
-          console.error("BranchContent -- handleSelectFolder(): ", err),
-        )
-    }
-  }, [folderPath, folderItemsSwitch]) */
-
-  const handleContentClicks = () => {}
-
   const handleSelectFolder = (e: any) => {
     console.log("dname: ", e.target.id)
     const fp = folderPath ? [...folderPath, e.target.id] : [e.target.id]
@@ -180,38 +132,42 @@ const BranchContent = ({
 
   return (
     <>
-    {
-      folders.length === 0 && files.length === 0 && (<SEmptyRepo>Empty Repo. Upload files or create modules to get started.</SEmptyRepo>)
+      {folders.length === 0 &&
+      files.length === 0 &&
+      !show &&
+      menuOption !== "DATASET" ? (
+        <SEmptyRepo>
+          Empty Repo. Upload files or create modules to get started.
+        </SEmptyRepo>
+      ) : repoEntity && selectedDataset === null ? (
+        <RepoSelectDataset repoEntity={repoEntity} selectedDataset={selectedDataset} setSelectedDataset={setSelectedDataset}/>
+      ) : subsets.length > 0 ? (
+          subsets.map((subset: any) => {
+            return (
+              <SubsetCard
+                key={subset.subset_id}
+                subset={subset}
+                dataset={selectedDataset}
+              />
+            )
+          })
+      ) : <></>
     }
-     {/*  {folders &&
-        folders.map((folder: any, index: number) => {
-          console.log('folder'+ index+ ": ", folder)
-          return (
-            <SRow
-              key={index}
-              className={index === folders.length - 1 ? "last" : ""}
-            >
-              <SContentLink onClick={handleSelectFolder}>
-                <SIcon id={folder} icon={faFolder} />
-                <SLine id={folder}>{folder}</SLine>
-              </SContentLink>
 
-              {folder.commit && <SCommit>{folder.commit}</SCommit>}
-              {folder.commit_message && (
-                <SCommitMsg>{folder.commit_message}</SCommitMsg>
-              )}
-              {folder.last_updated && (
-                <SLastUpdate>{folder.last_updated}</SLastUpdate>
-              )}
-            </SRow>
-          )
-        })} */}
       {files &&
         files.map((file: any, index: number) => {
           return (
             <SRow
               key={index}
-              className={index === files.length - 1 && index === 0 ? "first last" : index === files.length - 1 ? "last" : index === 0 ? "first" : ""}
+              className={
+                index === files.length - 1 && index === 0
+                  ? "first last"
+                  : index === files.length - 1
+                  ? "last"
+                  : index === 0
+                  ? "first"
+                  : ""
+              }
             >
               <SContentLink onClick={() => {}}>
                 <SIcon icon={faFile} />
@@ -231,6 +187,5 @@ const BranchContent = ({
     </>
   )
 }
-
 
 export default BranchContent
